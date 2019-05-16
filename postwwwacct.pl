fefd4f64523155816ac0436f349b1c25ce8440bd
Adam Walter <adam@vtldesign.com>
2019-05-16 14:08:38 -0400

    Removes some commands from postwwwacct

--- postwwwacct.pl
+++ postwwwacct.pl
@@ -8,7 +8,5 @@ if ($user eq "") {
 	exit;
 }
 
+# Install Bash Git Prompt
 system("git clone https://github.com/magicmonty/bash-git-prompt.git /home/$user/.bash-git-prompt --depth=1");
-system("curl \"https://gitlab.com/adamwalter/dotfiles/raw/master/.git-prompt-colors.sh\" -o /home/$user/.git-prompt-colors.sh");
-system("echo '' >> /home/$user/.bash_profile");
-system("echo 'source ~/.bash-git-prompt/gitprompt.sh' >> /home/$user/.bash_profile");
